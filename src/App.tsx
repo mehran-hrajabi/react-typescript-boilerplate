043c3900ad9944c2986f4ab58efef57eefd052d5
m.rajabi <mehran.hrajabi98@gmail.com>
2021-10-14 23:04:12 +0330

    Add redux (redux toolkit)

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,21 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { store } from './redux/store';
 import Landing from './components/Landing/Landing';
 import DummyComponent from './components/DummyComponent/DummyComponent';
 import './assets/styles/global.scss';
 
 const App: React.FC = () => {
 	return (
-		<BrowserRouter>
-			<Switch>
-				<Route path='/' component={Landing} exact />
-				<Route path='/hello' component={DummyComponent} />
-			</Switch>
-		</BrowserRouter>
+		<Provider store={store}>
+			<BrowserRouter>
+				<Switch>
+					<Route path='/' component={Landing} exact />
+					<Route path='/hello' component={DummyComponent} />
+				</Switch>
+			</BrowserRouter>
+		</Provider>
 	);
 };
 
